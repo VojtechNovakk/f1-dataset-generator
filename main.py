375652d74efeefa54042a5ce6d2e861963159b2e
VojtechNovak <vojta.novak2005@gmail.com>
2026-06-18 15:37:41 +0200

    feat: add remaining information and generate dataset

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import fastf1
 import os
 import numpy as np
 import pandas as pd
+import time
 
 os.makedirs('cache', exist_ok=True)
 fastf1.Cache.enable_cache('cache')
 
 all_races_data = []
 
-for year in range(2024, 2025):
+for year in range(2020, 2027):
     schedule = fastf1.get_event_schedule(year)
     for _, event in schedule.iterrows():
         if(event["EventFormat"] == "testing"):
@@ -16,11 +17,20 @@ for year in range(2024, 2025):
         location = event["Location"]
         round_num = event['RoundNumber']
         try:
+            time.sleep(1)
+            q_session = fastf1.get_session(year, round_num, 'Q')
+            q_session.load(telemetry=False, weather=False)
+            q_results = q_session.results[['Abbreviation', 'Q1', 'Q2', 'Q3']].copy()
+            q_results['Best_Q_Time'] = q_results[['Q1', 'Q2', 'Q3']].min(axis=1)
+            pole_time_sec = q_results['Best_Q_Time'].min().total_seconds()
+            q_results['Quali_Gap_Ratio'] = q_results['Best_Q_Time'].dt.total_seconds() / pole_time_sec
+            q_features = q_results[['Abbreviation', 'Quali_Gap_Ratio']]
+
             session = fastf1.get_session(year, round_num, 'R')
-            session.load()
+            session.load(telemetry=False, weather=True)
             results = session.results[
                 ['Abbreviation', 'TeamName', 'GridPosition', 'Position', 'Status', 'Time', 'Points']].copy()
-
+            results = results.merge(q_features, on='Abbreviation', how='left')
             weather = session.weather_data
             avg_track_temp = weather['TrackTemp'].mean()
             avg_air_temp = weather['AirTemp'].mean()
@@ -40,12 +50,14 @@ for year in range(2024, 2025):
                         gap_sec = row['Time'].total_seconds()
                         results.at[index, 'TotalTime_sec'] = winner_time + gap_sec
             results['Time_Ratio'] = results['TotalTime_sec'] / winner_time
+            results['Is_DNF'] = (~results['Status'].str.contains('Finished|Lap', regex=True, na=False)).astype(int)
             results['Year'] = year
             results['RoundNumber'] = round_num
             results['Location'] = location
             all_races_data.append(results)
+            print(f"Successfully processed: {year} - {location}")
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Failed to process {year} - {location}. Error: {e}")
 
 if all_races_data:
     final_df = pd.concat(all_races_data, ignore_index=True)
@@ -66,12 +78,22 @@ if all_races_data:
         on=['TeamName', 'Year', 'RoundNumber'],
         how='left'
     )
-    final_df.dropna(subset=['Time_Ratio', 'Driver_Form_5Races'], inplace=True)
+    final_df['Driver_DNF_Rate'] = (
+        final_df.groupby('Abbreviation')['Is_DNF']
+        .transform(lambda x: x.shift(1).rolling(window=10, min_periods=1).mean())
+    )
+    final_df.dropna(subset=['Time_Ratio', 'Driver_Form_5Races', 'Team_Form_5Races', 'Driver_DNF_Rate', 'Quali_Gap_Ratio'], inplace=True)
     final_df = pd.get_dummies(final_df, columns=['Location'], dtype=int)
-    circuit_columns = [col for col in final_df.columns if col.startswith('Location_')]
-    final_features = ['GridPosition', 'TrackTemp', 'AirTemp', 'IsRain', 'Time_Ratio', 'Driver_Form_5Races', 'Team_Form_5Races'] + circuit_columns
-    matrix_ready_df = final_df[final_features]
-    matrix_ready_df.to_csv('f1_matrix_data.csv', index=False)
-    print("\nDataset úspěšně vyčištěn a uložen do 'f1_matrix_data.csv'!")
+    circuit_columns = [col for col in final_df.columns if col.startswith('Location_') and col != 'Location_Monza']
+    final_features = ['GridPosition', 'Quali_Gap_Ratio', 'TrackTemp', 'AirTemp', 'IsRain', 'Driver_Form_5Races', 'Team_Form_5Races', 'Driver_DNF_Rate'] + circuit_columns + ['Time_Ratio']
+    matrix_ready_df = final_df[final_features].copy()
+    cols_to_normalize = ['GridPosition', 'TrackTemp', 'AirTemp', 'Driver_Form_5Races', 'Team_Form_5Races', 'Quali_Gap_Ratio', 'Driver_DNF_Rate']
+    for col in cols_to_normalize:
+        col_min = matrix_ready_df[col].min()
+        col_max = matrix_ready_df[col].max()
+        if col_max > col_min:
+            matrix_ready_df[col] = (matrix_ready_df[col] - col_min) / (col_max - col_min)
+    matrix_ready_df.to_csv('f1_matrix_data.csv', index=False, sep=';')
+    print("\nDataset successfully cleaned, normalized, and saved to 'f1_matrix_data.csv'!")
 else:
-    print("Nebyla nalezena žádná data ke sloučení.")
+    print("No data available to merge and process.")
